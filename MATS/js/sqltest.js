68a07e0c3c95a3f601486547e1f2a8c68a79bac6
Husain Mandliwala <husainmandliwala@gmail.com>
2018-04-30 15:53:21 -0400

    New

---
+++ MATS/js/sqltest.js
@@ -0,0 +1,52 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+// Create connection to database
+var config = 
+   {
+     userName: 'mats@matsmain', // update me
+     password: 'Path1234', // update me
+     server: 'matsmain.database.windows.net', // update me
+     options: 
+        {
+            encrypt: true,
+            database: 'mats_user' //update me
+        }
+   }
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) 
+   {
+     if (err) 
+       {
+          console.log(err)
+       }
+    else
+       {
+            console.log("ok")
+           //queryDatabase()
+       }
+   }
+ );
+
+function queryDatabase()
+   { console.log('Reading rows from the Table...');
+
+       // Read all rows from table
+     request = new Request(
+          "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+             function(err, rowCount, rows) 
+                {
+                    console.log(rowCount + ' row(s) returned');
+                    process.exit();
+                }
+            );
+
+     request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+         });
+             });
+     connection.execSql(request);
+   }
